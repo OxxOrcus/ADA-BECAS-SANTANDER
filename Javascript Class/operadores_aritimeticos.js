abfdc8ac4537a4fa98f1f7cdeb6d6fed8f18e2c6
Paulo Brocco <70035777+OxxOrcus@users.noreply.github.com>
2023-11-13 20:47:14 -0300

    New class JS

---
+++ Javascript Class/operadores_aritimeticos.js
@@ -0,0 +1,27 @@
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+// adicao +
+// Subtracao -
+// Multiplicacao *
+// Divisao /
+// Expoonenciacao **
+// Resto da divisao %
+
+const soma = primeiroNumero + segundoNumero
+
+console.log("Soma =", soma)
+console.log("Subtracao =", primeiroNumero - segundoNumero)
+console.log("Multiplicacao =", primeiroNumero * segundoNumero)
+console.log("Divisao =", primeiroNumero / segundoNumero)
+console.log("Expoonenciacao =", primeiroNumero ** segundoNumero)
+console.log("Resto da divisao =", primeiroNumero % segundoNumero)
+
+// Precedencia de operadores
+
+console.log((3 + 7) * 3)
+console.log((10 + 7 + 8) / 3)
+
+// Biblioteca Math
+
+console.log(Math.PI)
---
+++ Javascript Class/variaveis.js
@@ -0,0 +1,24 @@
+//VARIAVEIS
+
+let idade = 26;
+
+console.log(idade)
+
+//TIPOS DE VARIAVEIS
+
+idade = 30;               //number
+let numero = 30;          //Number
+let altura = 1.77         //Number
+let nome = "Walisson"     //String
+let estudando = true      // Boolean
+
+
+console.log(numero, typeof numero)
+console.log(altura)
+console.log(nome)
+console.log(estudando)
+
+
+const constante = 100;   //Valor atribuido nao pode ser alterado
+
+
